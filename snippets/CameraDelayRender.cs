fbf8be81b2c5c93c5d59e22a914ea822e227995e
DX4D <DX4Dgames@gmail.com>
2018-02-16 20:28:06 -0600

    DX4D Addons v105

---
+++ snippets/CameraDelayRender.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDelayRender : MonoBehaviour {
+    public float FPS = 5f;
+    public Camera renderCam;
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("Render", 0f, 1f / FPS);
+    }
+    void OnDestroy()
+    {
+        //CancelInvoke ();
+    }
+    void Render()
+    {
+        renderCam.enabled = true;
+    }
+    void OnPostRender()
+    {
+        renderCam.enabled = false;
+    }
+}
